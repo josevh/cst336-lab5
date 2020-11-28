166a0f8813e309261c7e001a430b6750e5251e50
Jose Herrera <josevictorherrera@gmail.com>
2020-11-27 20:36:22 -0800

    Add default timestamp

--- migrations/01_initial.js
+++ migrations/01_initial.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE `favorites` (`id` int(9) unsigned NOT NULL AUTO_INCREMENT, `imageUrl` varchar(255) NOT NULL, `keyword` varchar(255) NOT NULL, `created` datetime NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
+    "up": "CREATE TABLE `favorites` (`id` int(9) unsigned NOT NULL AUTO_INCREMENT, `imageUrl` varchar(255) NOT NULL, `keyword` varchar(255) NOT NULL, `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
     "down": "DROP TABLE favorites"
 }
