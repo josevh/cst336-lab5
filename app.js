4a57a5edabb021a0c0441f6c8874c3addf3d0f9a
Jose Herrera <josevictorherrera@gmail.com>
2020-11-27 20:36:09 -0800

    Add api endpoints

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const app = express()
-const fetch = require('node-fetch')
+const fetch = require("node-fetch")
+const mysql = require("mysql")
 
-const IP = process.env.IP || '0.0.0.0'
+const IP = process.env.IP || "0.0.0.0"
 const PORT = process.env.PORT || 5000
 
 const UNSPLASH_ACCESS_KEY = "VHn6YR4Lu7j-95tdOSGf02xw05FIFJH2FMenqQvddQE"
 const UNSPLASH_SECRET_KEY = "eUSCekM-sVifHouK16UXVxogYCUTfCfvr0eNB_r8TQ0"
 
 // express setup
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static("public"))
 app.use(expressLayouts)
+
+// functions
+var mysqlConn = null
+function getMysqlConn() {
+    if (mysqlConn === null) {
+        mysqlConn = mysql.createConnection({
+            host: process.env.MYSQL_HOST || "db",
+            user: process.env.MYSQL_USER || "root",
+            password: process.env.MYSQL_PASSWORD || "root",
+            database: process.env.MYSQL_DATABASE || "cst336-lab5"
+        })
+    }
+
+
+    return mysqlConn
+}
 
 // routes
 app.get("/", async function (req, res) {
@@ -48,6 +65,69 @@ app.get("/search", async function (req, res) {
         }
     }
     res.render("results", viewData)
+})
+
+app.get("/api/favorite", function (req, res) {
+    let conn = getMysqlConn()
+
+    conn.query(`SELECT id, imageUrl, keyword, created FROM favorites`, function (error, results, fields) {
+        if (error) {
+            return res.status(500).json()
+        }
+
+        let rows = JSON.parse(JSON.stringify(results[0]));
+        return res.status(200).json({
+            success: true,
+            data: rows
+        })
+    })
+})
+
+app.post("/api/favorite", function (req, res) {
+    let conn = getMysqlConn()
+
+    if (!('keyword' in req.body) || !('url' in req.body)) {
+        // TODO: error
+    }
+
+    let keyword = req.body.keyword
+    let url = req.body.url
+
+    conn.query(`INERT INTO favorites (imageUrl, keyword)
+                VALUES (?, ?)`, [url, keyword], function (error, results, fields) {
+        if (error) {
+            return res.status(500).json()
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: {id: results.insertId}
+        })
+    })
+})
+
+app.delete("/api/favorite/:id", function (req, res) {
+    let id = req.params.id
+
+    if (isNaN(id)) {
+        return res.status(400).json({success: false})
+    }
+
+    let conn = getMysqlConn()
+
+    conn.query(`DELETE FROM favorites where id = ?`, [id,], function (error, results, fields) {
+        if (error) {
+            return res.status(500).json()
+        }
+        // ID not found
+        if (results.changedRows === 0) {
+            return res.status(404).json()
+        }
+
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 
